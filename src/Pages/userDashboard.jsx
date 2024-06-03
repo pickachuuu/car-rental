e23f8b57448dffab45506d08d24efcfa7050bc2c
Derek <chrisDerekBerbo@gmail.com>
2024-06-03 19:33:03 +0800

    added user dashboard

---
+++ src/Pages/userDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const userDashboard = () => {
+  return (
+    <div>
+        <h1>Hello User!</h1>
+    </div>
+  );
+};
+
+export default userDashboard;
