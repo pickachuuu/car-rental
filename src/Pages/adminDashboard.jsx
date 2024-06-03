1723f689c58494f7a299cd43b163819fbb640285
Derek <chrisDerekBerbo@gmail.com>
2024-06-03 19:32:36 +0800

    added admin dashboard

---
+++ src/Pages/adminDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const adminDashboard = () => {
+  return (
+    <div>
+        <h1>Hello Admin!</h1>
+    </div>
+  );
+};
+
+export default adminDashboard;
