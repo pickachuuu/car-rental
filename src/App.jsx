164aeaf77261fcb693fd77a3bf0306e44f530372
Derek <dberb789@gmail.com>
2024-06-08 02:24:38 +0800

    Added private routes and imported user and admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './Pages/landingPage';
+import PrivateRoutes from './utils/privateRoutes';
+import AdminDashboard from './Pages/adminDashboard'
+import UserDashboard from './Pages/userDashboard'
+
 // Assuming you have a separate Login component in a file named Login.jsx
 import './App.css';
 
@@ -8,6 +12,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route element={<PrivateRoutes/>} >
+          <Route element={<AdminDashboard/>} path='/Dashboard' exact/>
+          <Route element={<UserDashboard/>} path='/Home'/>
+        </Route>
+
+
         <Route path="/" element={<Landing />} />
       </Routes>
     </Router>
