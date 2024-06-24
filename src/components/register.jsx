71e6d473911b068663c17573547f0125bcfc733a
Derek <dberb789@gmail.com>
2024-06-25 03:53:27 +0800

    refractor: changed function name from 'handleSubmit' to 'onSubmit' to avoid conflict with react-hook-form

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
 
 const Register = ({ toggleComponent }) => {
-  const [regCred, setRegCred] = useState({email: '', password: '', firstName: '', lastName: '', birthDate: '', cPassword: ''});
 
-  const handleformChanges = (event) => {
-    const name = event.target.name
-    const value = event.target.value
-    setRegCred((prevCred) => {
-      return {...prevCred, [name]: value}
-    })
-  }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+
+  const onSubmit = async (data) => {
     if (regCred.password === regCred.cPassword != 0){
       try{
         const response = await axios.post(`http://localhost:5000/api/register`, regCred)
         if (response.status == 200){
-          
+          // placeholder //
         }
       }catch(error){
-
+        // placeholder //
       }
     }else{
       console.log("Password doesnt match!")
@@ -39,19 +34,20 @@ const Register = ({ toggleComponent }) => {
           <div>
             <h1 className='d-none d-md-block'>Create your account</h1>
           </div>
+          
           <div className='mt-4'>
-            <form method='POST'>
-              <div className='row'>
+            <form method='POST' className='needs-validation' onSubmit={handleSubmit}>
+              <div className='form-row'>
                 <div className='col-6'>
                   <div className='mb-2'>
-                    <label className='form-label'>First name</label>
-                    <input type="text" className='form-control' name='firstName' onChange={handleformChanges}/>
+                    <label className='form-label'  >First name</label>
+                    <input type="text" className='form-control' name='firstName' id='validationCustom02' onChange={handleformChanges} required/>
                   </div>
                 </div>
                 <div className='col-6'>
                   <div className='mb-2'>
                     <label className='form-label'>Last name</label>
-                    <input type='text' className='form-control' name='lastName' onChange={handleformChanges}/>
+                    <input type='text' className='form-control' name='lastName' onChange={handleformChanges} required/>
                   </div>
                 </div>
               </div>
@@ -59,32 +55,32 @@ const Register = ({ toggleComponent }) => {
                 <div className='col-6'>
                   <div className='mb-2'>
                     <label className='form-label'>Email</label>
-                    <input type='email' className='form-control' name='email' onChange={handleformChanges}/>
+                    <input type='email' className='form-control' name='email' onChange={handleformChanges} required/>
                   </div>
                 </div>
                 <div className='col-6'>
                   <div className='mb-2'>
                     <label className='form-label'>birthdate</label>
-                    <input type='date' className='form-control' name='birthDate' onChange={handleformChanges}/>
+                    <input type='date' className='form-control' name='birthDate' onChange={handleformChanges} required/>
                   </div>
                 </div>
               </div>
               <div className='mb-2'>
                 <label className='form-label'>Password</label>
-                <input type='password' className='form-control' name='password' onChange={handleformChanges}/>
+                <input type='password' className='form-control' name='password' onChange={handleformChanges} required/>
               </div>
               <div className='mb-2'>
                 <label className='form-label'>Confirm Password</label>
-                <input type='password' className='form-control' name='cPassword' onChange={handleformChanges}/>
+                <input type='password' className='form-control' name='cPassword' onChange={handleformChanges} required/>
               </div>
               <div>
-                <button className='btn btn-dark btn-lg my-3 btn-block rounded-pill' onClick={handleSubmit}>
+                <button className='btn btn-dark btn-lg my-3 btn-block rounded-pill' type='submit'>
                   Create Account
                 </button>
                 <hr></hr>
                 <div className='text-center'>
                   <a>Already have an account? </a> 
-                  <a className='text-custom'  onClick={toggleComponent}>Login</a>
+                  <a className='text-custom' href='#' onClick={toggleComponent}>Login</a>
                 </div>
               </div>
             </form>
