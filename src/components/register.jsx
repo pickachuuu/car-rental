bc1ed286f8deac4b7acd26e8fc942b083c7e2b6f
Derek <dberb789@gmail.com>
2024-06-29 15:55:53 +0800

    refractor: Removed unused import

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
 
 const Register = ({ toggleComponent, setActiveComponent }) => {
