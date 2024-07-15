a7232f9d06406234d4d441656d939c3711981af9
Derek <dberb789@gmail.com>
2024-07-15 19:44:04 +0800

    feat: Implement private routes component (Resolved context access issue)

--- src/utils/privateRoutes.jsx
+++ src/utils/privateRoutes.jsx
@@ -1,12 +1,19 @@
-import { Outlet, Navigate } from 'react-router-dom';
+  import { useContext } from 'react';
+  import { Outlet, Navigate } from 'react-router-dom';
+  import { AuthTokenContext } from './authTokenContext';
 
-// get or check for existing token and verify //
+  // get or check for existing token and verify //
 
-const privateRoutes = () => {
-  let auth = {'token': true}
-  return (
-    auth.token ? <Outlet/> : <Navigate to ="/"/>
-  );
-};
+  const privateRoutes = () => {
+    const {token, setToken} = useContext(AuthTokenContext)
 
-export default privateRoutes;
+   // Check for token and redirect if not authenticated
+    if (!token) {
+      return <Navigate to="/" replace />; // Redirect to login on unauthorized access
+    }
+
+    // If token exists, render the child routes (Outlet)
+    return <Outlet />;
+  };
+
+  export default privateRoutes;
