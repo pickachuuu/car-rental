d9f26aaa20559cd291b9c15ca3515cb8ecbc4418
Derek <dberb789@gmail.com>
2024-07-21 20:18:58 +0800

    feat: Implement navbar component

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,41 +2,37 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='container-fluid bg-custom'>
-      <nav class="navbar navbar-expand-lg navbar-light bg-custom ">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-md navbar-light">
+  <div class="container">
+    <a class="navbar-brand" href="/">Car Rental</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse p-0" id="navbarSupportedContent" style={{ padding: 0 }}>
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Profile</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Deal</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Services
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div class="collapse navbar-collapse" id="navbarsExample05">
+      <ul class="navbar-nav ml-auto"> {/* Centered Links */}
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+
+
+      </ul>
+      <ul class="navbar-nav ml-auto"> {/* Right-side Login */}
+        <li class="nav-item">
+          <a class="nav-link" href="/Login">Login</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+
   );
 };
 
 export default NavBar;
-
